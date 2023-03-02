06e0c7cffbaa5c35c14213d28f68e7f619823514
Ak4zh <agwl.akash@gmail.com>
2023-03-02 18:12:08 +0530

    added authjs and prisma

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,7 +1,14 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GitHub from '@auth/core/providers/github';
 import { GITHUB_ID, GITHUB_SECRET } from '$env/static/private';
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+
+const prisma = new PrismaClient()
 
 export const handle = SvelteKitAuth({
-	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })]
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	providers: [GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })],
+	adapter: PrismaAdapter(prisma),
 });
