8b71e5209b2b623dd7152e88ed96afd6f9aa3e31
Ak4zh <agwl.akash@gmail.com>
2023-03-06 09:28:56 +0530

    minor improvements

--- src/lib/components/UserSettings.svelte
+++ src/lib/components/UserSettings.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
 	import { List, ListItem, BlockTitle, Button } from 'konsta/svelte';
 	import SignOutButton from './SignOutButton.svelte';
@@ -9,6 +8,13 @@
 	<BlockTitle>Account Info</BlockTitle>
 	<List strong>
 		<ListItem title={$page.data.session?.user?.name} subtitle={$page.data.session?.user?.email}>
+			<img
+				class="rounded-lg w-20"
+				slot="media"
+				src="{$page.data.session.user.image}"
+				width="80"
+				alt="demo"
+			/>
 			<SignOutButton slot="after"/>
 		</ListItem>
 	</List>
